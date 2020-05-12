7373de8ed46709b28eae25456c2ff682934808e2
Chaston Harper <chastonharper1@gmail.com>
2020-05-12 18:48:44 -0500

    added name and displayPosition function, also listed interests

---
+++ app.js
@@ -0,0 +1,26 @@
+let name = 'Name: Chaston Harper'
+let upper = name.charAt(0).toUpperCase() + name.slice(1);
+console.log(upper)
+
+function displayPosition(company, title, description) {
+    return company
+}
+
+let companyName = displayPosition ('Career: Video Editor');
+console.log(companyName);
+
+let jobTitle = displayPosition ('Title: Editor');
+console.log(jobTitle);
+
+let description = displayPosition ('Description: I like pie');
+console.log(description);
+
+console.log('My Interests:');
+
+let myInterests = {
+    tv: 'Fall TV',
+    cars: 'locomotives',
+    awkward: 'observing awkward situations',
+};
+
+console.log(myInterests.tv);
